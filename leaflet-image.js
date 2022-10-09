70521a8f53ad5b9da0aecaade2af042970568956
Graham Stewart <grahamsz@gmail.com>
2022-10-09 07:36:35 -0600

    removing cache busting

--- leaflet-image.js
+++ leaflet-image.js
@@ -250,11 +250,9 @@ module.exports = function leafletImage(map, callback) {
     }
 
     function addCacheString(url) {
-        // If it's a data URL we don't want to touch this.
-        if (isDataURL(url) || url.indexOf('mapbox.com/styles/v1') !== -1) {
-            return url;
-        }
-        return url + ((url.match(/\?/)) ? '&' : '?') + 'cache=' + cacheBusterDate;
+        // remove any cache busting
+        return url;
+     //   return url + ((url.match(/\?/)) ? '&' : '?') + 'cache=' + cacheBusterDate;
     }
 
     function isDataURL(url) {
